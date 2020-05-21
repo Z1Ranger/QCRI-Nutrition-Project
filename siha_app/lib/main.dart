7aa00c3737086dc971592ec8737794c1bf0b7cfe
Z1Ranger <gvvarma2712@gmail.com>
2020-05-21 04:03:08 -0400

    added food displaying functionality

--- siha_app/lib/main.dart
+++ siha_app/lib/main.dart
@@ -3,11 +3,22 @@ import 'package:flutter/material.dart';
 import 'package:speech_recognition/speech_recognition.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter_plugin_pdf_viewer/flutter_plugin_pdf_viewer.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
 
 List<String> entries = <String>[];
 List<String> uniqueEntries = <String>[];
 List<String> time = <String>[];
 List<String> uniqueTime = <String>[];
+List<String> mealTime = <String>[];
+List<String> uniqueMealTime = <String>[];
+List<String> meals = <String>['Breakfast', 'Lunch', 'Dinner', 'Snack'];
+List<String> foodPhotos = <String>[];
+List<String> foodNames = <String>[];
+
+final nutrition_app_id = '34b3b309';
+final nutrition_app_key = '90be06c4d2e994fa3ef87d4106729004';
 
 void main() {
   runApp(MainPage());
@@ -113,6 +124,7 @@ class FoodInput extends StatefulWidget {
 class _FoodInputState extends State<FoodInput> {
   var _textController = TextEditingController();
   bool pressed = false;
+  bool imagePressed = false;
 
   SpeechRecognition _speechRecognition;
   bool _isAvailable = false;
@@ -123,9 +135,24 @@ class _FoodInputState extends State<FoodInput> {
   DateTime _dateTime = DateTime.now();
   TimeOfDay _timeOfDay = TimeOfDay.now();
 
+  String _meal;
+
+  void setMeal() {
+    if (5 < _timeOfDay.hour && _timeOfDay.hour < 11) {
+      _meal = meals[0];
+    } else if (12 < _timeOfDay.hour && _timeOfDay.hour < 15) {
+      _meal = meals[1];
+    } else if (18 < _timeOfDay.hour && _timeOfDay.hour < 21) {
+      _meal = meals[2];
+    } else {
+      _meal = meals[3];
+    }
+  }
+
   @override
   void initState() {
     super.initState();
+    setMeal();
     initSpeechRecognizer();
   }
 
@@ -163,6 +190,29 @@ class _FoodInputState extends State<FoodInput> {
         );
   }
 
+  getData() async {
+    foodPhotos = [];
+    foodNames = [];
+    http.Response response = await http.post(
+      Uri.encodeFull('https://trackapi.nutritionix.com/v2/natural/nutrients'),
+      headers: {
+        'x-app-id': nutrition_app_id,
+        'x-app-key': nutrition_app_key,
+        'x-remote-user-id': '0'
+      },
+      body: {'query': '$_textController.text'},
+    );
+//    print(response.body);
+    dynamic data = jsonDecode(response.body);
+//    print(data['foods']);
+
+    for (int i = 0; i < data['foods'].length; i++) {
+      foodPhotos.add(data['foods'][i]['photo']['thumb']);
+      foodNames.add(data['foods'][i]['food_name']);
+    }
+    return 'success - showing food';
+  }
+
   @override
   Widget build(BuildContext context) {
     _textController.text = widget.text;
@@ -176,119 +226,167 @@ class _FoodInputState extends State<FoodInput> {
         centerTitle: true,
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              DateTime(
-                _dateTime.year,
-                _dateTime.month,
-                _dateTime.day,
-                _timeOfDay.hour,
-                _timeOfDay.minute,
-              ).toString(),
-            ),
-            RaisedButton(
-              child: Text('Pick a Date and Time'),
-              onPressed: () {
-                showTimePicker(
-                  context: context,
-                  initialTime: TimeOfDay(minute: 0, hour: 0),
-                ).then(
-                  (time) {
-                    setState(
-                      () {
-                        if (time != null) {
-                          _timeOfDay = time;
-                        }
-                      },
-                    );
-                  },
-                );
-                showDatePicker(
-                  context: context,
-                  initialDate: DateTime.now(),
-                  firstDate: DateTime(2000),
-                  lastDate: DateTime(2100),
-                ).then(
-                  (date) {
-                    setState(
-                      () {
-                        if (date != null) {
-                          _dateTime = date;
-                        }
-                      },
-                    );
-                  },
-                );
-              },
-            ),
-            ListTile(
-              title: TextField(
-                controller: _textController,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                DateTime(
+                  _dateTime.year,
+                  _dateTime.month,
+                  _dateTime.day,
+                  _timeOfDay.hour,
+                  _timeOfDay.minute,
+                ).toString(),
               ),
-            ),
-            ListTile(
-              title: RaisedButton(
-                child: Text('Send'),
+              RaisedButton(
+                child: Text('Pick a Date and Time'),
                 onPressed: () {
-                  setState(
-                    () {
-                      pressed = true;
-                      entries.insert(0, _textController.text);
-                      time.insert(
-                        0,
-                        DateTime(
-                          _dateTime.year,
-                          _dateTime.month,
-                          _dateTime.day,
-                          _timeOfDay.hour,
-                          _timeOfDay.minute,
-                        ).toString(),
+                  showTimePicker(
+                    context: context,
+                    initialTime: TimeOfDay(minute: 0, hour: 0),
+                  ).then(
+                    (time) {
+                      setState(
+                        () {
+                          if (time != null) {
+                            _timeOfDay = time;
+                            setMeal();
+                          }
+                        },
                       );
-                      if (!uniqueEntries.contains(_textController.text)) {
-                        uniqueEntries.insert(0, _textController.text);
-                        uniqueTime.insert(
-                          0,
-                          DateTime(
-                                  _dateTime.year,
-                                  _dateTime.month,
-                                  _dateTime.day,
-                                  _timeOfDay.hour,
-                                  _timeOfDay.minute)
-                              .toString(),
-                        );
-//                      _dateTime.toString()
-                      }
                     },
                   );
-//                  var route = MaterialPageRoute(
-//                    builder: (context) =>
-//                        (FoodHistory(value: _textController.text)),
-//                  );
+                  showDatePicker(
+                    context: context,
+                    initialDate: DateTime.now(),
+                    firstDate: DateTime(2020),
+                    lastDate: DateTime(2100),
+                  ).then(
+                    (date) {
+                      setState(
+                        () {
+                          if (date != null) {
+                            _dateTime = date;
+                          }
+                        },
+                      );
+                    },
+                  );
                 },
               ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            ListTile(
-              title: pressed ? Text(_textController.text) : SizedBox(),
-            ),
-            ListTile(
-              title: pressed
-                  ? Text(
-                      DateTime(_dateTime.year, _dateTime.month, _dateTime.day,
-                              _timeOfDay.hour, _timeOfDay.minute)
-                          .toString(),
-                    )
-                  : SizedBox(),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
+              RaisedButton(
+                child: Text(_meal),
+                onPressed: () {
+                  setState(() {
+                    int index = meals.indexOf(_meal) + 1;
+                    _meal = meals[index % 4];
+                  });
+                },
+              ),
+              SizedBox(
+                height: 15,
+              ),
+//              ListTile(
+//                title: TextField(
+//                  controller: _textController,
+//                ),
+//              ),
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(20, 0, 0, 0),
+                      child: TextField(
+                        controller: _textController,
+                      ),
+                    ),
+                  ),
+                  FlatButton(
+                    shape: CircleBorder(),
+                    child: Icon(
+                      Icons.mic,
+                      color: Colors.blue[800],
+                    ),
+                    onPressed: () {
+                      if (_isAvailable && !_isListening)
+                        _speechRecognition
+                            .listen(locale: "en_US")
+                            .then((result) => print('$result'));
+                    },
+                  ),
+                ],
+              ),
+              ListTile(
+                title: RaisedButton(
+                  child: Text('Send'),
+                  onPressed: () {
+                    setState(
+                      () {
+                        pressed = true;
+                        entries.insert(0, _textController.text);
+                        time.insert(
+                          0,
+                          DateTime(
+                            _dateTime.year,
+                            _dateTime.month,
+                            _dateTime.day,
+                            _timeOfDay.hour,
+                            _timeOfDay.minute,
+                          ).toString(),
+                        );
+                        mealTime.insert(0, _meal);
+                        if (!uniqueEntries.contains(_textController.text)) {
+                          uniqueEntries.insert(0, _textController.text);
+                          uniqueTime.insert(
+                            0,
+                            DateTime(
+                              _dateTime.year,
+                              _dateTime.month,
+                              _dateTime.day,
+                              _timeOfDay.hour,
+                              _timeOfDay.minute,
+                            ).toString(),
+                          );
+                          uniqueMealTime.insert(0, _meal);
+                        }
+                      },
+                    );
+                  },
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              FlatButton(
+                child: Icon(Icons.image),
+                onPressed: () {
+                  getData().then(
+                    (val) {
+                      setState(
+                        () {
+                          print(val);
+                          imagePressed = true;
+                        },
+                      );
+                    },
+                  );
+                },
+              ),
+//              ListTile(
+//                title: pressed ? Text(_textController.text) : SizedBox(),
+//              ),
+//              ListTile(
+//                title: pressed
+//                    ? Text(
+//                        DateTime(_dateTime.year, _dateTime.month, _dateTime.day,
+//                                _timeOfDay.hour, _timeOfDay.minute)
+//                            .toString(),
+//                      )
+//                    : SizedBox(),
+//              ),
+              Container(
 //              mainAxisAlignment: MainAxisAlignment.center,
 //              children: <Widget>[
 //                FloatingActionButton(
@@ -306,16 +404,18 @@ class _FoodInputState extends State<FoodInput> {
 //                          );
 //                  },
 //                ),
-              child: FloatingActionButton(
-                child: Icon(Icons.mic),
-                onPressed: () {
-                  if (_isAvailable && !_isListening)
-                    _speechRecognition
-                        .listen(locale: "en_US")
-                        .then((result) => print('$result'));
-                },
-                backgroundColor: Colors.blue[400],
-              ),
+//                child: FlatButton(
+//                  child: Icon(
+//                    Icons.mic,
+//                    color: Colors.blue[800],
+//                  ),
+//                  onPressed: () {
+//                    if (_isAvailable && !_isListening)
+//                      _speechRecognition
+//                          .listen(locale: "en_US")
+//                          .then((result) => print('$result'));
+//                  },
+//                ),
 //                FloatingActionButton(
 //                  heroTag: null,
 //                  child: Icon(Icons.stop),
@@ -331,8 +431,33 @@ class _FoodInputState extends State<FoodInput> {
 //                  },
 //                ),
 //              ],
-            ),
-          ],
+                  ),
+              imagePressed
+                  ? ListView.builder(
+                      padding: const EdgeInsets.all(8),
+                      itemCount: foodPhotos.length,
+                      scrollDirection: Axis.vertical,
+                      shrinkWrap: true,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Image.network(
+                              foodPhotos[index],
+                              width: 50,
+                              height: 50,
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Text(foodNames[index]),
+                          ],
+                        );
+                      },
+                    )
+                  : SizedBox()
+            ],
+          ),
         ),
       ),
     );
@@ -362,7 +487,7 @@ class _FoodHistoryState extends State<FoodHistory> {
         itemBuilder: (BuildContext context, int index) {
           return ListTile(
             title: FlatButton(
-              color: Colors.blue[200],
+              color: Colors.blue[300],
               onPressed: () {
                 Navigator.push(
                   context,
@@ -379,9 +504,22 @@ class _FoodHistoryState extends State<FoodHistory> {
                 ),
               ),
             ),
-            subtitle: Container(
-              child: Text(uniqueTime[index]),
-              color: Colors.blue[300],
+            subtitle: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    child: Text(uniqueTime[index]),
+                    color: Colors.blue[200],
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    child: Text(uniqueMealTime[index]),
+                    color: Colors.blue[100],
+                  ),
+                ),
+              ],
             ),
           );
         },
@@ -454,22 +592,6 @@ class Home extends StatelessWidget {
         ],
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-//        floatingActionButton: UnicornDialer(
-//          parentButtonBackground: Colors.blue[400],
-//          orientation: UnicornOrientation.VERTICAL,
-//          parentButton: Icon(Icons.add),
-//          childButtons: _getProfileMenu(),
-//        ),
-//        floatingActionButton: SpeedDial(
-//          animatedIcon: AnimatedIcons.menu_home,
-//          children: [
-//            SpeedDialChild(
-//              child: Icon(Icons.ac_unit),
-//              label: 'First item',
-//              onTap: () {},
-//            ),
-//          ],
-//        ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.push(
@@ -490,17 +612,18 @@ class Home extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
             IconButton(
-                icon: Icon(
-                  Icons.room_service,
-                  size: 30,
-                  color: Colors.white,
-                ),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => FoodHistory()),
-                  );
-                }),
+              icon: Icon(
+                Icons.room_service,
+                size: 30,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => FoodHistory()),
+                );
+              },
+            ),
             IconButton(
               icon: Icon(
                 Icons.explore,
