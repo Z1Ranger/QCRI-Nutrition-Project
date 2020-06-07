a2d2c5c414dbc57a90bf5e5ba7e13e4dd5f133d2
Z1Ranger <gvvarma2712@gmail.com>
2020-06-07 05:42:32 -0400

    modified code

--- Nutritional_analysis_flask.py
+++ Nutritional_analysis_flask.py
@@ -3,14 +3,42 @@ import requests
 from keys import *  # initialize app-id and app-key by client in file named keys
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
-
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:vishnu@localhost:5432/nutrition"
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 api_url_base = 'https://trackapi.nutritionix.com/v2/'
 headers = {'x-app-id': nutrition_app_id,      # protecting client app-id
            'x-app-key': nutrition_app_key,    # protecting client app-key
            'x-remote-user-id': '0'}
+
+
+class NutrientModel(db.Model):
+    __tablename__ = 'n_info'
+
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String())
+    tot_cals = db.Column(db.String())
+    tot_carbs = db.Column(db.String())
+    tot_proteins = db.Column(db.String())
+    tot_fats = db.Column(db.String())
+
+
+
+    def __init__(self, date, tot_cals, tot_carbs, tot_proteins, tot_fats):
+        self.date = date
+        self.tot_cals = tot_cals
+        self.tot_carbs = tot_carbs
+        self.tot_proteins = tot_proteins
+        self.tot_fats = tot_fats
+
+    def __repr__(self):
+        return f"<Date {self.date}>"
 
 
 @app.route('/food', methods=['POST'])
@@ -42,6 +70,7 @@ def get_info():
 
                 food_dict['foods'].append({'food': food_list[i]['food_name'],
                                            'img': food_list[i]['photo']['thumb'],
+                                           'cals': food_list[i]['nf_calories'],
                                            'fats': food_list[i]['nf_total_fat'],
                                            'carbs': food_list[i]['nf_total_carbohydrate'],
                                            'proteins': food_list[i]['nf_protein'],
@@ -61,7 +90,37 @@ def get_info():
         return None
 
 
-# food_consumed = input()
+@app.route('/nutrient_upload', methods=['POST'])
+def handle_nutrients():
+    if request.is_json:
+        data = request.get_json()
+        n_info = NutrientModel(date=data['date'], tot_cals=data['tot_cals'], tot_carbs=data['tot_carbs'], tot_proteins=data['tot_proteins'], tot_fats=data['tot_fats'])
+        db.session.add(n_info)
+        db.session.commit()
+        return {"message": f"Date {n_info.date} has been created successfully."}
+    else:
+        return {"error": "The request payload is not in JSON format"}
+
+
+@app.route('/nutrient_info', methods=['POST'])
+def nutrients_date():
+    if request.is_json:
+        date = request.get_json()['date']
+        nutrient_by_date = NutrientModel.query.filter_by(date = date).all()
+        tot_cal = 0
+        tot_carb = 0
+        tot_fat = 0
+        tot_protein = 0
+        for n_meal in nutrient_by_date:
+            tot_cal += int(n_meal.tot_cals)
+            tot_carb += int(n_meal.tot_carbs)
+            tot_protein += int(n_meal.tot_proteins)
+            tot_fat += int(n_meal.tot_fats)
+
+        return {'tot_cals': tot_cal, 'tot_carbs': tot_carb, 'tot_proteins': tot_protein,'tot_fats': tot_fat}
+    else:
+        return {"error": "The request payload is not in JSON format"}
+
 
 
 if __name__ == "__main__":
